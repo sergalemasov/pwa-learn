be972de062cbb9b03a9cd4b57a42bcbdcfb9ef08
Sergey Alemasov <s.alemasov@tinkoff.ru>
2020-04-11 00:23:39 +0400

    some additions

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,24 @@
 self.addEventListener('install', event => {
     console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open('static')
+            .then(cache => {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                ]);
+            })
+    )
 });
 
 self.addEventListener('activate', event => {
@@ -8,7 +27,18 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-    console.log('[Service Worker] Fetching something ...', event);
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => (
+                cachedResponse ||
+                fetch(event.request)
+                    .then(response => caches.open('dynamic')
+                        .then(cache => {
+                            cache.put(event.request, response.clone());
 
-    event.respondWith(fetch(event.request));
+                            return response;
+                        })
+                    )
+            ))
+    );
 });
