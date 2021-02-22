797f10ef0034de9f1ffcd7dc5e0945edaf458f65
Alemasov Sergey Aleksandrovich <s.alemasov@tinkoff.ru>
2021-02-22 19:23:01 +0400

    lalala

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express');
+const { join } = require('path');
+
+const PORT = process.env.PORT || 5000;
+const DIST_FOLDER = join(process.cwd(), 'public');
+
+const app = express();
+
+app.get('*.*', express.static(DIST_FOLDER));
+
+app.use(createProxyMiddleware('/rabota', {
+    target: 'https://rabota.tinkoff.ru/',
+    changeOrigin: true,
+    secure: true,
+    pathRewrite: { '^/rabota' : '' }
+}));
+
+app.get('/', (_req, res) => {
+    res.sendFile(join(DIST_FOLDER, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}!`);
+});
