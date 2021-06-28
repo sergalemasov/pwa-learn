57b5d83737b8bd125dc2b5a20bcd7aeca3ab9ee0
Alemasov Sergey Aleksandrovich <s.alemasov@tinkoff.ru>
2021-06-28 16:44:54 +0300

    some fixes for sw

--- public/sw.js
+++ public/sw.js
@@ -7,20 +7,20 @@ self.addEventListener('install', event => {
         caches.open(CACHE_STATIC_NAME)
             .then(cache => {
                 console.log('[Service Worker] Precaching App Shell');
-                cache.addAll([
-                    '/feed',
-                    '/help',
-                    '/rabota',
-                    '/src/js/app.js',
-                    '/src/js/feed.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    '/src/images/main-image.jpg',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-                ]);
+                // cache.addAll([
+                //     '/feed',
+                //     '/help',
+                //     '/rabota',
+                //     '/src/js/app.js',
+                //     '/src/js/feed.js',
+                //     '/src/js/material.min.js',
+                //     '/src/css/app.css',
+                //     '/src/css/feed.css',
+                //     '/src/images/main-image.jpg',
+                //     'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                //     'https://fonts.googleapis.com/icon?family=Material+Icons',
+                //     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                // ]);
             })
     )
 });
